10e83d162a74c08234741560021dff65a33160ba
Mahamudul Haque Noor <mahamudulhaquenoor@gmail.com>
2022-11-27 14:48:32 +0600

    users delete api created

--- index.js
+++ index.js
@@ -137,6 +137,14 @@ async function run() {
             res.send(user)
         })
 
+        // users delete api
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await usersCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
         
